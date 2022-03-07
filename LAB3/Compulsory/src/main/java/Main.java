a06d60e9c9f6b2cfea31aca60643c87a5cee9b4a
stefanarnautu <stefanarnautu01@gmail.com>
2022-03-07 19:10:44 +0200

    .

--- LAB3/Compulsory/src/main/java/Main.java
+++ LAB3/Compulsory/src/main/java/Main.java
@@ -3,12 +3,18 @@ public class Main {
 
         Network net=new Network();
 
-        Computer computeA=new Computer("Computer A","MAC","Tg.Neamt","20.30.40.54:2024");
+        Computer computeA=new Computer("computerB","MAC","Tg.Neamt","20.30.40.54:2024");
         Router routerA=new Router("routerA","MAC","Tg.Neamt","11.45.32.40:2000");
         Switch switchA=new Switch("switchA","Tg.Neamt","23.54.71.45:2067");
+        Computer computeB=new Computer("computerB","MAC","Tg.Neamt","20.30.40.54:2024");
+        Router routerB=new Router("routerB","MAC","Tg.Neamt","11.45.32.40:2000");
+        Switch switchB=new Switch("switchB","Tg.Neamt","23.54.71.45:2067");
         net.addNodes(computeA);
         net.addNodes(routerA);
         net.addNodes(switchA);
+        net.addNodes(computeB);
+        net.addNodes(routerB);
+        net.addNodes(switchB);
 
         net.afiseazaObiectele();
     }
