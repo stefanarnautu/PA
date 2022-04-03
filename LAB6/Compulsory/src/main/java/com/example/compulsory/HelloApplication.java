c960fdd030906c132c0864aeb1761ab5221c540c
stefanarnautu <stefanarnautu01@gmail.com>
2022-04-03 15:28:47 +0300

    ..

--- LAB6/Compulsory/src/main/java/com/example/compulsory/HelloApplication.java
+++ LAB6/Compulsory/src/main/java/com/example/compulsory/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
         stage.setTitle("My Game");
         stage.setScene(scene);
         stage.show();
